4c0501ff71d7d17a4118ae0e611ef326bf55371e
Nathan Young <NathanY3G@users.noreply.github.com>
2021-05-21 19:42:59 +0200

    Fixed imports

--- pioemu/instructions/__init__.py
+++ pioemu/instructions/__init__.py
@@ -11,13 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from .common import next_instruction
 from .jmp import (
     jmp,
     jmp_when_x_is_non_zero_and_post_decrement,
     jmp_when_y_is_non_zero_and_post_decrement,
 )
-from .mov import mov_into_isr, mov_into_osr, mov_into_pins, mov_into_x, mov_into_y
 from .out import out_null, out_pindirs, out_pins, out_x, out_y
 from .pull import pull_blocking, pull_nonblocking
-from .set import set_pins, set_pindirs, set_x, set_y
 from .wait import wait_for_gpio_low, wait_for_gpio_high
