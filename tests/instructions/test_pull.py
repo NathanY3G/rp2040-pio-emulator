4e57677d0802c56bdfba0a1edfaf6aade9712d0c
Nathan Young <77929198+NathanY3G@users.noreply.github.com>
2023-04-10 19:04:44 +0200

    Refactor tests for PULL to improve consistency

--- tests/instructions/test_pull.py
+++ tests/instructions/test_pull.py
@@ -1,4 +1,4 @@
-# Copyright 2021, 2022 Nathan Young
+# Copyright 2021, 2022, 2023 Nathan Young
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,44 +17,37 @@ import pytest
 
 from pioemu import ShiftRegister, State
 
-from ..support import emulate_single_instruction
+from ..support import emulate_single_instruction, instruction_param
 
-
-def test_pull_loads_osr_from_non_empty_fifo():
-    initial_state = State(transmit_fifo=deque([0x1111_1111]))
-
-    new_state = emulate_single_instruction(0x8080, initial_state)  # pull noblock
-
-    assert new_state.output_shift_register == ShiftRegister(0x1111_1111, 0)
-
-
-def test_pull_copies_x_to_osr_when_nonblocking_and_fifo_empty():
-    initial_state = State(x_register=0x2222_2222)
-
-    new_state = emulate_single_instruction(0x8080, initial_state)  # pull noblock
-
-    assert new_state.output_shift_register == ShiftRegister(0x2222_2222, 0)
-
-
-def test_pull_stalls_when_blocking_and_fifo_empty():
-    initial_state = State(output_shift_register=ShiftRegister(0x3333_3333, 0))
-
-    new_state = emulate_single_instruction(0x80A0, initial_state)  # pull block
-
-    assert new_state.program_counter == 0
-    assert new_state.output_shift_register == ShiftRegister(0x3333_3333, 0)
-
-
-@pytest.mark.parametrize(
-    "opcode",
-    [
-        pytest.param(0x8080, id="pull noblock"),
-        pytest.param(0x80A0, id="pull block"),
-    ],
-)
-def test_pull_clears_the_output_shift_register(opcode: int):
-    initial_state = State(transmit_fifo=deque([0]))
-
+@pytest.mark.parametrize("opcode, initial_state, expected_state", [
+    instruction_param(
+        "pull noblock when fifo not empty",
+        0x8080,
+        State(transmit_fifo=deque([0x1111_1111])),
+        State(transmit_fifo=deque(), output_shift_register=ShiftRegister(0x1111_1111, 0)),
+    ),
+    instruction_param(
+        "pull noblock when fifo empty",
+        0x8080,
+        State(transmit_fifo=deque(), x_register=0x2222_2222),
+        State(transmit_fifo=deque(), output_shift_register=ShiftRegister(0x2222_2222, 0), x_register=0x2222_2222),
+    ),
+    instruction_param(
+        "pull block when fifo not empty",
+        0x80A0,
+        State(transmit_fifo=deque([0x3333_3333])),
+        State(transmit_fifo=deque(), output_shift_register=ShiftRegister(0x3333_3333, 0)),
+    ),
+    instruction_param(
+        "pull block when fifo empty",
+        0x80A0,
+        State(transmit_fifo=deque()),
+        State(transmit_fifo=deque()),
+        expected_program_counter=0,
+    ),
+])
+# fmt: on
+def test_pull_instruction(opcode: int, initial_state: State, expected_state: State):
     new_state = emulate_single_instruction(opcode, initial_state)
 
-    assert new_state.output_shift_register.counter == 0
+    assert new_state == expected_state
