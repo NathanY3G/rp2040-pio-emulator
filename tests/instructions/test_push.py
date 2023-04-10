4ad1107bf0d20542cad3218a63016692a574f825
Nathan Young <77929198+NathanY3G@users.noreply.github.com>
2023-04-10 19:06:48 +0200

    Add tests for PUSH (#91)
    * Add tests for PUSH
    
    * Refactor tests for PULL to improve consistency

--- tests/instructions/test_push.py
+++ tests/instructions/test_push.py
@@ -1,4 +1,4 @@
-# Copyright 2021, 2022, 2023 Nathan Young
+# Copyright 2023 Nathan Young
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,35 +19,26 @@ from pioemu import ShiftRegister, State
 
 from ..support import emulate_single_instruction, instruction_param
 
+
+# fmt: off
+@pytest.mark.skip(reason="Pull request 60 has not been merged yet")
 @pytest.mark.parametrize("opcode, initial_state, expected_state", [
     instruction_param(
-        "pull noblock when fifo not empty",
-        0x8080,
-        State(transmit_fifo=deque([0x1111_1111])),
-        State(transmit_fifo=deque(), output_shift_register=ShiftRegister(0x1111_1111, 0)),
+        "push noblock",
+        0x8000,
+        State(input_shift_register=ShiftRegister(72, 8)),
+        State(input_shift_register=ShiftRegister(0, 0), receive_fifo=deque([72])),
     ),
     instruction_param(
-        "pull noblock when fifo empty",
-        0x8080,
-        State(transmit_fifo=deque(), x_register=0x2222_2222),
-        State(transmit_fifo=deque(), output_shift_register=ShiftRegister(0x2222_2222, 0), x_register=0x2222_2222),
-    ),
-    instruction_param(
-        "pull block when fifo not empty",
-        0x80A0,
-        State(transmit_fifo=deque([0x3333_3333])),
-        State(transmit_fifo=deque(), output_shift_register=ShiftRegister(0x3333_3333, 0)),
-    ),
-    instruction_param(
-        "pull block when fifo empty",
-        0x80A0,
-        State(transmit_fifo=deque()),
-        State(transmit_fifo=deque()),
+        "push block",
+        0x8020,
+        State(input_shift_register=ShiftRegister(72, 8), receive_fifo=deque([108, 200, 288, 392])),
+        State(input_shift_register=ShiftRegister(72, 8), receive_fifo=deque([108, 200, 288, 392])),
         expected_program_counter=0,
     ),
 ])
 # fmt: on
-def test_pull_instruction(opcode: int, initial_state: State, expected_state: State):
+def test_push_instruction(opcode: int, initial_state: State, expected_state: State):
     new_state = emulate_single_instruction(opcode, initial_state)
 
     assert new_state == expected_state
