1d56c710a5b16b9deb50d441f1165f006143d258
Nathan Young <77929198+NathanY3G@users.noreply.github.com>
2023-06-10 16:22:16 +0200

    Improve tests for the input source feature (#107)
    Co-authored-by: Nathan Young <NathanY3G@users.noreply.github.com>

--- tests/test_emulation.py
+++ tests/test_emulation.py
@@ -13,10 +13,7 @@
 # limitations under the License.
 import pytest
 
-from pioemu import State, clock_cycles_reached, emulate
-
-from .opcodes import Opcodes
-from .support import emulate_single_instruction
+from pioemu import emulate
 
 
 def test_stop_when_requires_value():
@@ -27,33 +24,3 @@ def test_stop_when_requires_value():
 def test_emulation_stops_when_unsupported_opcode_is_reached():
     with pytest.raises(StopIteration):
         next(emulate([0xE0E0], stop_when=lambda opcode, _: False))
-
-
-def test_pin_values_follow_input_source():
-    input_values_over_time = [0x0000_FFFF, 0xFFFF_0000, 0xFFFF_FFFF]
-
-    pin_values_series = [
-        state.pin_values
-        for _, state in emulate(
-            [Opcodes.nop()],
-            stop_when=clock_cycles_reached(len(input_values_over_time)),
-            input_source=lambda clock: input_values_over_time[clock],
-        )
-    ]
-
-    assert pin_values_series == input_values_over_time
-
-
-def test_input_source_does_not_impact_output_pins():
-    initial_state = State(pin_directions=0xFFFF_0000, pin_values=0)
-
-    _, new_state = next(
-        emulate(
-            [Opcodes.nop()],
-            stop_when=clock_cycles_reached(1),
-            initial_state=initial_state,
-            input_source=lambda _: 0xFFFF_FFFF,
-        )
-    )
-
-    assert new_state.pin_values == 0x0000_FFFF
