65efda7b27485e2ffdcaa2427bcbdc931422d9a3
Nathan Young <NathanY3G@users.noreply.github.com>
2023-10-24 07:33:31 +0200

    Blackened code

--- pioemu/emulation.py
+++ pioemu/emulation.py
@@ -81,7 +81,9 @@ def emulate(
 
         if input_source:
             masked_values = current_state.pin_values & current_state.pin_directions
-            masked_input = input_source(current_state.clock) & ~current_state.pin_directions
+            masked_input = (
+                input_source(current_state.clock) & ~current_state.pin_directions
+            )
             current_state = replace(
                 current_state,
                 pin_values=masked_values | masked_input,
