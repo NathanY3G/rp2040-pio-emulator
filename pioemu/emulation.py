09cf28289f705954f52699d2193d221886b7a8b4
Nathan Young <NathanY3G@users.noreply.github.com>
2024-01-11 08:44:43 +0100

    Support type-hints on user provided input_sources
    This change encourages but does not mandate the use of type
    annotations/hints on user provided input sources.

--- pioemu/emulation.py
+++ pioemu/emulation.py
@@ -1,4 +1,4 @@
-# Copyright 2021, 2022, 2023 Nathan Young
+# Copyright 2021, 2022, 2023, 2024 Nathan Young
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import inspect
+import logging
 from dataclasses import replace
 from typing import Callable, Generator, List, Tuple
 
@@ -25,7 +27,7 @@ def emulate(
     *,
     stop_when: Callable[[int, State], bool],
     initial_state: State | None = None,
-    input_source: Callable[[int], int] | None = None,
+    input_source: Callable[[State], int] | Callable[[int], int] | None = None,
     shift_isr_right: bool = True,
     shift_osr_right: bool = True,
     side_set_base: int = 0,
@@ -45,6 +47,8 @@ def emulate(
         Predicate used to determine if the emulation should stop or continue.
     initial_state : State, optional
         Initial values to use.
+    input_source : Callable, optional
+        Invoked before each instruction to obtain the values currently present on the GPIO pins.
     shift_isr_right : bool, optional
         Shift the Input Shift Reigster (ISR) to the right when True and to the left when False.
     shift_osr_right : bool, optional
@@ -65,8 +69,17 @@ def emulate(
     -------
     generator
     """
+    logger = logging.getLogger(__name__)
+
     if stop_when is None:
         raise ValueError("emulate() missing value for keyword argument: 'stop_when'")
+
+    if input_source and not _is_input_source_signature_supported(input_source):
+        logger.warning(
+            "input_source is missing the expected type annotations/hints and may not work as expected"
+        )
+
+        input_source = _wrap_deprecated_input_source(input_source)
 
     shift_isr_method = (
         ShiftRegister.shift_right if shift_isr_right else ShiftRegister.shift_left
@@ -89,9 +102,7 @@ def emulate(
 
         if input_source:
             masked_values = current_state.pin_values & current_state.pin_directions
-            masked_input = (
-                input_source(current_state.clock) & ~current_state.pin_directions
-            )
+            masked_input = input_source(current_state) & ~current_state.pin_directions
             current_state = replace(
                 current_state,
                 pin_values=masked_values | masked_input,
@@ -138,6 +149,20 @@ def emulate(
         current_state = replace(current_state, clock=current_state.clock + 1)
 
         yield (previous_state, current_state)
+
+
+def _is_input_source_signature_supported(input_source: Callable):
+    parameter_types = [
+        parameter.annotation
+        for parameter in inspect.signature(input_source).parameters.values()
+    ]
+
+    return parameter_types == [State]
+
+
+def _wrap_deprecated_input_source(input_source: Callable[[int], int]):
+    print("Wrapping!")
+    return lambda state: input_source(state.clock)
 
 
 def _advance_program_counter(
