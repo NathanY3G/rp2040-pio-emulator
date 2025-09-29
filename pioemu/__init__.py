ff12fe9ef665f0b2239e3b49f62241d9030bab0e
Nathan Young <77929198+NathanY3G@users.noreply.github.com>
2025-09-29 21:45:17 +0200

    Bump version in preparation for next release (#178)

--- pioemu/__init__.py
+++ pioemu/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2021, 2022, 2023 Nathan Young
+# Copyright 2021, 2022, 2023, 2024, 2025 Nathan Young
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-__version__ = "0.85.0"
+__version__ = "0.86.0"
 
 from .conditions import clock_cycles_reached
 from .emulation import emulate
